2f7501edc7eec0e9f1ac738a6448a6f4e140025f
Henning Dahlheim <hactar@cyberkraft.ch>
2020-12-22 09:55:59 +0100

    Clone alpine elements after liveview patch

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,13 +16,20 @@ import 'phoenix_html'
 import { Socket } from 'phoenix'
 import NProgress from 'nprogress'
 import { LiveSocket } from 'phoenix_live_view'
-import 'alpinejs'
+import Alpine from 'alpinejs'
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute('content')
 let liveSocket = new LiveSocket('/live', Socket, {
   params: { _csrf_token: csrfToken },
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from.__x) {
+        Alpine.clone(from, to)
+      }
+    },
+  },
 })
 
 // Show progress bar on live navigation and form submits
